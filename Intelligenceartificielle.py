8c4534f303f6763636ef9c9533442fecc2f5a0f1
Cyprien Dujardin <103560167+CyprienDujardin@users.noreply.github.com>
2022-04-13 10:53:20 +0200

    Create Intelligenceartificielle.py

---
+++ Intelligenceartificielle.py
@@ -0,0 +1,25 @@
+#L'algorithme n'est pas terminé il sera mis en ligne fois complété, ci dessous le protocole en suivant la méthode de backpropagation pour une classification
+
+
+#On cherche à classifier des données en probabilité d'os cassé ou non 
+
+    
+#lis les données dans un fichier 
+#on sépare les données entrainement/ test
+
+#on définit un nombre de réseau 
+
+
+#on définit aléatoirement les poids (entre -1 et 1)(Tableu de dimention des données)
+
+ #on entraine le modèle 
+    #on calcul la sortie du sigmode
+
+    #On calcul l'erreur
+    #on la corrige avec la rétropropagation 
+    #on applique le correction sur les poids
+    
+    
+#on test les data séparées 
+
+#on compte le nombre de succès d'échec et on compare
